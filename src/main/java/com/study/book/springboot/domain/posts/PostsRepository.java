afc5f08f0b6d75d3863a17243874220ac58546d2
freelife <freelfe1191.good@gmail.com>
2020-12-31 04:18:36 +0900

    4.4 전체 조회 화면 만들기

--- src/main/java/com/study/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/study/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.study.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by KMS on 2020/12/29.
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/study/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.book.springboot.service.posts;
 
 import com.study.book.springboot.domain.posts.Posts;
 import com.study.book.springboot.domain.posts.PostsRepository;
+import com.study.book.springboot.web.dto.PostsListResponseDto;
 import com.study.book.springboot.web.dto.PostsResponseDto;
 import com.study.book.springboot.web.dto.PostsSaveRequestDto;
 import com.study.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by KMS on 2020/12/30.
@@ -39,5 +43,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         
         return new PostsResponseDto(entity);
+    }
+    
+    @Transactional(readOnly = true) // readOnly 트랜잭션 범위는 유지 조회 기능만 남김, 조회 속도가 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
