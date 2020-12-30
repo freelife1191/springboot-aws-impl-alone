75e90fef226ee001b9ecdaa2fd1dc6681eec134e
freelife <freelfe1191.good@gmail.com>
2020-12-31 07:10:28 +0900

    5.4 어노테이션 기반으로 개선하기

--- src/main/java/com/study/book/springboot/web/IndexController.java
+++ src/main/java/com/study/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.study.book.springboot.web;
 
+import com.study.book.springboot.config.auth.LoginUser;
 import com.study.book.springboot.config.auth.dto.SessionUser;
 import com.study.book.springboot.service.posts.PostsService;
 import com.study.book.springboot.web.dto.PostsResponseDto;
@@ -19,11 +20,10 @@ public class IndexController {
     private final HttpSession httpSession;
     
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model,
+                        @LoginUser SessionUser user // 세션정보 가져오기 어느 컨트롤러든지 @LoginUser로 세션 정보를 가져올 수 있음
+        ) {
         model.addAttribute("posts", postsService.findAllDesc());
-        
-        // 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         
         // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
         // 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보임
