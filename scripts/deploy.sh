658049e9e2d672d7d0dae095fded790d06e21616
freelife <freelfe1191.good@gmail.com>
2020-12-31 21:48:11 +0900

    09. Travis CI 배포 자동화 - 코드가 푸시되면 자동으로 배포해 보자 9.6 CodeDeploy 로그 확인 까지 deploy.sh 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 # pgrep는 process id 추출 -f 옵션으로 프로세스 이름 찾기
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -41,4 +41,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
     -Dspring.profiles.active=real \
-    $REPOSITORY/$JAR_NAME 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
