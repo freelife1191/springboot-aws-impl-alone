73a6416c88421a4491252274a41a38925517310a
freelife <freelfe1191.good@gmail.com>
2020-12-31 21:08:29 +0900

    09. Travis CI 배포 자동화 - 코드가 푸시되면 자동으로 배포해 보자 9.5 배포 자동화 구성 까지

---
+++ scripts/step2_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step1
+PROJECT_NAME=springboot-aws-impl-alone
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+# pgrep는 process id 추출 -f 옵션으로 프로세스 이름 찾기
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+# 계속 구동될 수 있도록 nohup 로 구동
+# -Dspring.config.location 스프링 파일 위치를 지정
+# application-oauth.yml 은 외부에 파일이 있기 때문에 절대경로를 사용
+# -Dspring.profiles.active=real real profile 를 활성화 시킴
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
+    -Dspring.profiles.active=real \
+    $REPOSITORY/$JAR_NAME 2>&1 &
